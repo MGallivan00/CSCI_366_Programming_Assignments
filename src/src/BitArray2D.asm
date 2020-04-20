859a43e95fb25221c945d64c52046ed294341aff
MGallivan00 <54639309+MGallivan00@users.noreply.github.com>
2020-04-19 19:50:29 -0700

    Update BitArray2D.asm
    commented

--- src/src/BitArray2D.asm
+++ src/src/BitArray2D.asm
@@ -11,43 +11,54 @@ set_bit_elem:
         ; rdx contains row
         ; rcx contains col
 
-        mov rax, rdx        ; get i (row)
-        imul rax, rsi       ; compute row-width*row
-        add rax, rcx        ; compute row-width*row + column
-;-------------------------------------------------
+        ; basically the array is 8 times more than we expect from the rows since it holds bytes (8 bytes = 1 bit)
+        ; so we grab the byte and set the bit to 1
+        ; index = 1/8(row * row width + col)
+        ; byte_offset = index / 8
+        ; bit off_set = index - (byte_offset * 8)
+        ; mask = 1 << (8 - bit_offset - 1)
 
-        mov rcx, rax        ; index
-        sar rcx, 3          ; byte_offset = rcx = index/8
 
-        push rcx
-        mov rcx, rax
-        mov rbx, 1
-        and rcx, 7
-        shl rbx, cl
+        mov rax, rdx        ; row
+        imul rax, rsi       ; width
+        add rax, rcx        ; column
 
-        pop rcx
-        mov rax, [rdi + rcx]
-        or rax, rbx         ; set the bit into (row, col)
-        mov [rdi + rcx], rax
+        mov rcx, rax        ; copy index into rcx
+        sar rcx, 3          ; byte_offset = index/8
 
-    .done:
+        push rcx            ; save the byte_offset to the stack
+        mov rcx, rax        ; restore rcx to the index
+        mov rbx, 1          ; move 1 into a new register (prep the bit mask)
+        and rcx, 7          ; grab the last 3 bits (remainder from byte_offset division)
+                            ; these last 3 will have 1's (111 = 7) so anding will put the
+                            ; remainder into rcx
+        shl rbx, cl         ; shift the 1 by the remainder (in the upper register of rcx)
+                            ; positions the bit mask
+
+        pop rcx             ; pop the index into rcx
+        ; or rbx, [rdi + rcx]        ; grab proper bit from memory and set to 1
+        ; mov [rdi + rcx], rax       ; return the bit into memory
+
+        setl al             ; creates boolean
+        movzx rax, al       ; moves boolean into returned values
+
         mov rsp, rbp        ; restore stack pointer to before we pushed parameters onto the stack
         pop rbp             ; remove rbp from the stack to restore rsp to initial value
         ret                 ; return value in rax
 
 
-get_bit_elem2:
+get_bit_elem:
         push rbp            ; save the base pointer on the stack (at rsp+8)
         mov rbp, rsp        ; set up the rbp for the bottom of this frame
-        add rdi, rbp
-        mov rax, [rdi]
-        setl al
+       ; add rdi, rbp
+       ; mov rax, [rdi]     ; breaking with exit code 139 when above line is removed
+        setl al             ; this is a test driver to see why rdi was breaking
         movzx rax, al
         mov rsp, rbp        ; restore stack pointer to before we pushed parameters onto the stack
         pop rbp             ; remove rbp from the stack to restore rsp to initial value
         ret                 ; return value in rax
 
-get_bit_elem:
+get_bit_elem2:
         push rbp            ; save the base pointer on the stack (at rsp+8)
         mov rbp, rsp        ; set up the rbp for the bottom of this frame
 
@@ -56,19 +67,18 @@ get_bit_elem:
         ; rdx contains row
         ; rcx contains col
 
-        mov rax, rdx        ; get i (row)
-        imul rax, rsi       ; compute row-width*row
-        add rax, rcx        ; compute row-width*row + column
-;-------------------------------------------------
+        mov rax, rdx        ; this is a copy of set_bit_element
+        imul rax, rsi
+        add rax, rcx
 
-        mov rcx, rax        ; index
-        sar rcx, 3          ; byte_offset = rcx = index/8
+        mov rcx, rax
+        sar rcx, 3
 
-        push rax
-        mov rax, [rdi + rcx]
-        pop rcx
+       ;push rax
+       ;mov rax, [rdi + rcx]    ; this grabs the element from memory
+       ;pop rcx
 
-        mov rbx, 1
+        mov rbx, 1          ; this is a copy of set_bit_element
         and rcx, 7
         shl rbx, cl
 
